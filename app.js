6168b3594b29125f7bb7974c7a87e10eed6e7c4c
katega1234 <69979974+katega1234@users.noreply.github.com>
2020-08-20 22:13:20 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,15 +3,15 @@
 
 ///Edit Under This only
 
-var firebaseConfig = {
-  apiKey: "api-key",
-  authDomain: "project-id.firebaseapp.com",
-  databaseURL: "https://project-id.firebaseio.com",
-  projectId: "project-id",
-  storageBucket: "project-id.appspot.com",
-  messagingSenderId: "sender-id",
-  appId: "app-id",
-  measurementId: "G-measurement-id",
+const firebaseConfig = {
+  apiKey: "AIzaSyAK0Cv9Uq_nE1i4cQpqUg5UKdGaEfkX2_I",
+  authDomain: "instagram-f3d60.firebaseapp.com",
+  databaseURL: "https://instagram-f3d60.firebaseio.com",
+  projectId: "instagram-f3d60",
+  storageBucket: "instagram-f3d60.appspot.com",
+  messagingSenderId: "198817851731",
+  appId: "1:198817851731:web:0e69dd616f71b9e18b59a6",
+  measurementId: "G-KBSG4F0REJ"
 };
 
      
